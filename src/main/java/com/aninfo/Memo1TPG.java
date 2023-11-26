fa905a8758f33deb1dc0e5e005161d37b2f4b298
Denise-Peke <ddallacqua@fi.uba.ar>
2023-11-26 14:17:13 -0300

    agrego persistencia

--- src/main/java/com/aninfo/Memo1TPG.java
+++ src/main/java/com/aninfo/Memo1TPG.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -23,6 +24,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @RestController
 @SpringBootApplication
 @EnableSwagger2
+@EnableJpaRepositories
 
 public class Memo1TPG {
 
